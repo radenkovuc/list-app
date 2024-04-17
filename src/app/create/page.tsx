f5ef5dc8afe56581630600fb649562e016c6f9b9
Radenko Vucanovic <radenkovuc@gmail.com>
2024-04-17 10:42:50 +0200

    initial setup

---
+++ src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Create new item",
+    description: "Page for creating new item",
+};
+
+const Create = () => (
+    <div>Create</div>
+)
+
+export default Create
---
+++ src/app/details/[id]/page.tsx
@@ -0,0 +1,12 @@
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Item details",
+    description: "Page with item details",
+};
+
+const Details = () => (
+    <div>Details</div>
+)
+
+export default Details
